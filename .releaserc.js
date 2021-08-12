ceccf548a5a1b51d46f79580b25d8eb43a5c7d13
Jay Kariesch <jay@curbee.com>
2021-08-12 01:47:58 -0500

    fix(releaserc): removed tests
    removed test options

--- .releaserc.js
+++ .releaserc.js
@@ -1,29 +1,16 @@
 module.exports = {
     plugins: [
         ["@semantic-release/commit-analyzer", {
-            preset: "angular",
             parserOpts: {
                 noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
             }
         }],
         ["@semantic-release/release-notes-generator", {
-            preset: "angular",
             parserOpts: {
                 noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING", "CH"],
             },
             writerOpts: {
                 commitsSort: ["subject", "scope"],
-                transform: (str) => {
-                    if(/^ch: [0-9]/i.test(str) || /^ch:[0-9]/i.test(str)) {
-                        try {
-                            const id = str.split(':').slice(-1)[0].trim()
-                            return `CH: [${id}](https://app.clubhouse.io/curbee/story/${id})`
-                        } catch(_err) {
-                            return str
-                        }
-                    }
-                    return str
-                }
             }
         }],
         ["@semantic-release/npm", {
