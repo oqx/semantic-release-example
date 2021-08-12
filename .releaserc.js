d01d48c22f8f42c9d161641f6824ebc6840059db
Jay Kariesch <jay@curbee.com>
2021-08-11 23:46:28 -0500

    feat: add link to clubhouse
    adds a link to clubhouse if footer contains a clubhouse ID
    
    ch4922

---
+++ .releaserc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    plugins: [
+        ["@semantic-release/commit-analyzer", {
+            preset: "angular",
+            parserOpts: {
+                noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+            }
+        }],
+        ["@semantic-release/release-notes-generator", {
+            preset: "angular",
+            parserOpts: {
+                "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+            },
+            writerOpts: {
+                commitsSort: ["subject", "scope"],
+                transform: (commit) => {
+                    if(commit.footer && commit.footer.startsWith('[') && commit.footer.endsWith(']')) {
+                        commit.footer = commit.footer.replace('[', '').replace(']', '').replace(/ch/i, '')
+                    } else if(commit.footer.startsWith('ch')) {
+                        commit.footer = commit.footer.replace(/ch/i, '')
+                    }
+                    return {
+                        ...commit,
+                        footer: `[${commit.footer}](https://app.clubhouse.io/curbee/story/${commit.footer})`
+                    }
+                }
+            }
+        }],
+        ["@semantic-release/npm", {
+            npmPublish: false,
+            tarballDir: "dist",
+        }],
+        ["@semantic-release/github", {
+            assets: "dist/*.tgz"
+        }],
+        "@semantic-release/git",
+    ],
+    branch: "master",
+    preset: "angular"
+}
